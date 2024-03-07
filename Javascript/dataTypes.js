4e718df8acb9ef37424b43fe76e768f3ca16c01f
Aladesiun Temitope <83991100+aladesiun@users.noreply.github.com>
2024-03-08 00:44:06 +0100

    updates

---
+++ Javascript/dataTypes.js
@@ -0,0 +1,58 @@
+// data types 
+// var age = "my agae is 26"; 
+
+// strings, numbers, booleans
+// enums, null, undefined, 
+
+// for javascript
+// strings, numbers, booleans, object, array, null, undefined
+
+
+// others
+// for numbers-> int, float: int = 0-9 500.4, 1, float: 300, 5.9, double: 50.5
+
+
+// for javascript
+// strings, numbers, booleans, object, array, null, undefined
+
+
+// var age = "he" "llo";
+
+// var age = "hello";
+// var arryAge = [ 30, 5, 5 ,6, 7, 8, 9, 9, 50.0];
+
+// // arryAge.splice(0, 5);
+// console.log(arryAge.filter((arr)=> arr < 30 ));
+// // arryAge.length;
+
+// // arryAge.lastIndexOf(5);
+// console.log(arryAge.lastIndexOf(5));
+// // arryAge.join;
+
+// arryAge.splice(0, 5);
+// arryAge.splice(0, 5);
+
+// console.log(age.replace('-', '7000'));
+
+
+
+// Numbers
+// const age = 50.76433233;
+
+// console.log(age.toExponential());
+// console.log(age.toFixed());
+// console.log(age.toLocaleString());
+// console.log(age.toPrecision(6));
+// console.log(age.toString());
+// console.log(parseInt(age));
+// console.log(parseFloat(age));
+// console.log(JSON.stringify(age));
+
+// booleans
+// true or false
+const isLoading = true;
+
+if (isLoading) {
+    
+}
+console.log(isLoading);
---
+++ Javascript/index.js
@@ -0,0 +1,47 @@
+
+
+// js Basics
+// DOM 
+// algorithm
+
+
+// Variables
+// Methods
+// operations(logic, maths)
+// debug
+
+// VARIABLES
+    // ES6
+    // let
+    // const
+    // var
+
+var the_name = "tope";
+
+// alphanumeric
+// snake case
+// camel case
+// block case
+
+// alphanumeric
+var  name2 = "themmy";
+
+// snake case
+var the_name = "snake case var";
+
+// camel case
+var mabelBestVar = "mabel best";
+
+// block case
+
+const CAN_NOT_CHANGE = "can not change";
+
+var CAN_CHANGE = "cna change";
+
+CAN_CHANGE = "new vcalue";
+
+console.log(CAN_CHANGE);
+
+if (5 > 10) {
+    let 
+}
